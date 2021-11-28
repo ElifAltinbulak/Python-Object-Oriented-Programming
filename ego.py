61b254feac0bb76321a07af65d85100480414708
ElifAltinbulak <76043798+ElifAltinbulak@users.noreply.github.com>
2021-11-29 00:42:27 +0300

    Create ego.py

---
+++ ego.py
@@ -0,0 +1,25 @@
+class EGO:
+    def __init__(self,type,money):
+        self.type=type
+        self.money=money
+    def work(self):
+        if self.type=="öğrenci":
+            if self.money>=1.75:
+                self.money=self.money-1.75
+                print(f"Öğrenci kalan para:{self.money}")
+            elif self.money<=1.75:
+                print("Bakiye yok")
+        elif self.type=="tam":
+            if self.money>=3.25:
+                self.money=self.money-3.25
+                print(f"Tam kalan para:{self.money}")
+            elif self.money<=3.25:
+                print("Bakiye yok")
+        else:
+            print("Error")
+y1=EGO("öğrenci",50)
+y2=EGO("tam",1)
+y3=EGO("öğrenci",0)
+y1.work()
+y2.work()
+y3.work()
