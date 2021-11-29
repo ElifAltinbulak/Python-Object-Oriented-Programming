daeef897199ab51a8295eef7a7130933ca7bc3ee
ElifAltinbulak <76043798+ElifAltinbulak@users.noreply.github.com>
2021-11-29 20:01:55 +0300

    Update ego.py

--- ego.py
+++ ego.py
@@ -7,19 +7,22 @@ class EGO:
             if self.money>=1.75:
                 self.money=self.money-1.75
                 print(f"Öğrenci kalan para:{self.money}")
-            elif self.money<=1.75:
+            elif self.money<1.75:
                 print("Bakiye yok")
         elif self.type=="tam":
             if self.money>=3.25:
                 self.money=self.money-3.25
                 print(f"Tam kalan para:{self.money}")
-            elif self.money<=3.25:
+            elif self.money<3.25:
                 print("Bakiye yok")
         else:
             print("Error")
 y1=EGO("öğrenci",50)
 y2=EGO("tam",1)
 y3=EGO("öğrenci",0)
+y4=EGO("öğrenci",1.75)
 y1.work()
 y2.work()
 y3.work()
+y4.work()
+y4.work()
